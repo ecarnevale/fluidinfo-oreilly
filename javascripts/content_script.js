12f3f0c4b4e0b1a4fdeb664e8890c82b4e32db9e
Emanuel Carnevale <emanuel.carnevale@gmail.com>
2011-03-03 14:42:11 +0100

    jquery dialog + some cluetip

--- javascripts/content_script.js
+++ javascripts/content_script.js
@@ -1,6 +1,7 @@
 var book_title  = $("#title").html();
 var authors_count = $(".product-metadata .authorname").size();
 var icon_url = chrome.extension.getURL('images/icons/icon.png');
+var own_icon_url = chrome.extension.getURL('images/icons/own_icon.png');
 if(authors_count > 1){
   authors = [];
   $(".product-metadata .authorname").each(function(index){
@@ -19,8 +20,10 @@ function actOnBook(action){
       'action'      : action+'Book'
     },
     function(resp){
-      $("#fluidinfo-button-own").toggle();
-      $("#fluidinfo-button-release").toggle();
+      // mettere classi
+      $(".fluidinfo-button").toggle();
+      $(".own-notice").toggle();
+      $(".release-notice").toggle();
     });
 
 };
@@ -30,46 +33,64 @@ var popup_templ = '<h3>You can find <span id="book_title">\'{{title}}\'</span> o
 +'{{#google_url}}<p><a id="google_url" href="{{google_url}}">books.google.com</a></p>{{/google_url}}'
 +'{{#goodreads_url}}<p><a id="goodreads_url" href="{{goodreads_url}}">goodreads.com</a></p>{{/goodreads_url}}'
 +'<br/>'
-+'{{#owns}}You own this book.<br/><em>(<span class="own">click the icon if you don\'t have it anymore</span>)</em>{{/owns}}'
-+'{{^owns}}You don\'t own this book.<br><em>(<span class="release">click the icon if you have it</span>)</em>{{/owns}}';
++'<div class="own-notice" {{^owns}}style="display:none;"{{/owns}}>You own this book.<br/><em>(<span class="own">click the icon if you don\'t have it anymore</span>)</em></div>'
++'<div class="release-notice" {{#owns}}style="display:none;"{{/owns}}>You don\'t own this book.<br><em>(<span class="release">click the icon if you have it</span>)</em></div>';
 
-$(".product-metadata .authorname").parents("dl").after('<div id="fluidinfo"><div id="popup" style="display:none;"></div><div id="object_id" style="display:none;"></div><img id="fluidinfo-button" src="' + icon_url + '" alt=""/></div>');
+$(".product-metadata .authorname").parents("dl").after('<div id="fluidinfo"><div id="popup" style="display:none;"></div><div id="object_id" style="display:none;"></div><img id="fluidinfo-button-own" class="fluidinfo-button" src="' + own_icon_url + '" alt="#popup"/><img id="fluidinfo-button-release" class="fluidinfo-button" style="display:none;" src="' + icon_url + '" alt="#popup"/></div>');
 
 function setupTooltip(response){
-  $("#fluidinfo-button").qtip({
-    style: {
-      border: {
-         width: 2,
-         radius: 10
-      },
-      padding: 10, 
-      tip: true, // Give it a speech bubble tip with automatic corner detection
-      name: 'light' // Style it according to the preset 'cream' style
-    },
-    hide: {
-      delay: 200,
-      fixed: true
-    },
-    border: {
-             width: 3,
-             radius: 8,
-             color: '#6699CC'
-          },
-    content: $.mustache(popup_templ,
-                        {title: book_title,
-                         amazon_url: response.values["amazon.com/url"].value,
-                         google_url: response.values["books.google.com/url"].value,
-                         goodreads_url: response.values["goodreads.com/url"].value,
-                         amazon_price: "$" + response.values["amazon.com/price/usd"].value/100,
-                         owns: response.owns
-                        }),
-       position: {
-          corner: {
-             target: 'topRight',
-             tooltip: 'bottomLeft'
-          }
-       }
-    });
+  $("#popup").html($.mustache(popup_templ,
+                                         {title: book_title,
+                                          amazon_url: response.values["amazon.com/url"].value,
+                                          google_url: response.values["books.google.com/url"].value,
+                                          goodreads_url: response.values["goodreads.com/url"].value,
+                                          amazon_price: "$" + response.values["amazon.com/price/usd"].value/100,
+                                          owns: response.owns
+                                         }));
+  //$("#popup").dialog({ autoOpen: false });
+  $(".fluidinfo-button").cluetip({
+    cluetipClass: 'rounded',
+    fx: { open: 'fadeIn' },
+    showTitle: false,
+    sticky: true,
+    mouseOutClose: true,
+    local: true,
+    attribute: 'alt'
+  });
+  //$("#fluidinfo-button").qtip({
+    //style: {
+      //border: {
+         //width: 2,
+         //radius: 10
+      //},
+      //padding: 10, 
+      //tip: true, // Give it a speech bubble tip with automatic corner detection
+      //name: 'light' // Style it according to the preset 'cream' style
+    //},
+    //hide: {
+      //delay: 200,
+      //fixed: true
+    //},
+    //border: {
+             //width: 3,
+             //radius: 8,
+             //color: '#6699CC'
+          //},
+    //content: $.mustache(popup_templ,
+                        //{title: book_title,
+                         //amazon_url: response.values["amazon.com/url"].value,
+                         //google_url: response.values["books.google.com/url"].value,
+                         //goodreads_url: response.values["goodreads.com/url"].value,
+                         //amazon_price: "$" + response.values["amazon.com/price/usd"].value/100,
+                         //owns: response.owns
+                        //}),
+       //position: {
+          //corner: {
+             //target: 'topRight',
+             //tooltip: 'bottomLeft'
+          //}
+       //}
+    //});
 };
 
 chrome.extension.sendRequest(
@@ -97,7 +118,6 @@ chrome.extension.sendRequest(
       function(respTwo){
         if(respTwo.owns){
           resp.owns = true;
-        }else{
           $("#fluidinfo-button-own").hide();
           $("#fluidinfo-button-release").show();
         }
